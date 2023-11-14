0a0b902f40227912ccf7d9ddbdc6f4c95ea85256
hugheylewis <103709866+hugheylewis@users.noreply.github.com>
2023-11-14 11:04:21 -0500

    Update main.py
    Fix class instances and attributes. Remove hard-coded tenant IDs, app IDs, and app secrets.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from config import config
 import json
 import requests
 import csv
@@ -8,54 +9,49 @@ class Header:
 
     ACCEPT = '"accept": "application/json"'
 
-    def __init__(self, url, tenant_id, app_id, app_secret):
-        self._url = url
+    def __init__(self, tenant_id, app_id, app_secret, url):
         self._tenant_id = tenant_id
         self._app_id = app_id
         self._app_secret = app_secret
+        self._url = url
 
     @property
     def tenant_id(self):
-        return self.tenant_id
+        return self._tenant_id
 
     @property
     def app_id(self):
-        return self.app_id
+        return self._app_id
 
     @property
     def app_secret(self):
-        return self.app_secret
+        return self._app_secret
 
     @property
     def url(self):
         return self._url
 
     @url.setter
-    def url(self, new_endpoint):
-        if isinstance(new_endpoint, str) and "https://api-us.securitycenter.microsoft.com" in new_endpoint:
-            self._url += new_endpoint
-
-    def __str__(self):
-        return f"{self.ACCEPT}, 'Authorization': 'Bearer {self._access_key};secretKey={self._secret_key}'"
-
-    def asdict(self):
-        return {'accept': "application/json", 'X-ApiKeys': f'accessKey={self._access_key};secretKey={self._secret_key}'}
+    def url(self, new_url):
+        if isinstance(new_url, str):
+            self._url = new_url
 
 
 def aad_token():
-    tenant_id = ""
-    app_id = ""
-    app_secret = ""
-    url = f""
-    resource_app_id_uri = ''
+    tenant = config.APIkeys.tenant_id
+    token_header = Header(config.APIkeys.tenant_id, config.APIkeys.app_id, config.APIkeys.app_secret,
+                          url=f"https://login.microsoftonline.com/{tenant}/oauth2/token")
+    token_header.url = f"https://login.microsoftonline.com/{token_header.tenant_id}/oauth2/token"
+    resource_app_id_uri = 'https://api-us.securitycenter.microsoft.com'
+
     body = {
         'resource': resource_app_id_uri,
-        'client_id': app_id,
-        'client_secret': app_secret,
+        'client_id': token_header.app_id,
+        'client_secret': token_header.app_secret,
         'grant_type': 'client_credentials'
     }
 
-    req = requests.post(url, body)
+    req = requests.post(token_header.url, body)
     response = req.text
     json_response = json.loads(response)
     return json_response['access_token']
@@ -64,12 +60,17 @@ def aad_token():
 def get_machine_id(entra_token):
     ids_to_offboard = []
 
-    url = 'https://api-us.securitycenter.microsoft.com/api/machines'
+    tenant = config.APIkeys.tenant_id
+    get_machine_header = Header(config.APIkeys.tenant_id, config.APIkeys.app_id, config.APIkeys.app_secret,
+                          url=f"https://login.microsoftonline.com/{tenant}/oauth2/token")
+    get_machine_header.url = 'https://api-us.securitycenter.microsoft.com/api/machines'
+
     headers = {
-        'Content-Type': 'application/json',
+        'Content-Type': get_machine_header.ACCEPT,
         'Authorization': "Bearer " + entra_token
     }
-    req = requests.get(url, headers=headers)
+
+    req = requests.get(get_machine_header.url, headers=headers)
     response = req.text
     json_response = json.loads(response)
 
@@ -84,20 +85,22 @@ def offboard():
     initials = input("Enter your initials to claim responsibility for this action: ")
     bearer_token = aad_token()
     machine_id_list = get_machine_id(bearer_token)
-    eastern_dt = datetime.now()
+    current_dt = datetime.now()
+    
     headers = {
         'Content-Type': 'application/json',
         'Authorization': "Bearer " + bearer_token
     }
+    
     body = {
-        "Comment": f"Offboarded machine by automation at {eastern_dt}. {initials}."
+        "Comment": f"Offboarded machine by automation at {current_dt}. {initials}."
     }
+    
     if not machine_id_list:
         with open('devices.csv', 'r') as dfile:
             csv_reader = csv.reader(dfile)
             for index, row in enumerate(csv_reader):
                 if index == 1:
-                    # TODO: Iterate through entire Device ID column on CSV file, not just first cell (which is what row[0] does)
                     url = f'https://api-us.securitycenter.windows.com/api/machines/{row[0]}/offboard'
                     req = requests.post(url, headers=headers, json=body)
                     response = req.text
